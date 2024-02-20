c05a2e21b2803bd5a228a7e2ac887e8d6f836e85
YuriMSdS <yuri.md13@gmail.com>
2024-02-20 16:01:22 -0300

    alteração pós setting do .env e declaração de variável no código

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,5 +1,8 @@
 provider "google" {
-    credentials = file ("path/para/sua/credencial.json") #Como se trata da credencial da GCP, optei por utilizar variável de ambiente
+    credentials = file ("var.google_credentials_path") #Como se trata da credencial da GCP, optei por utilizar variável de ambiente
     project     = DevOpsProject #Aqui é a declaração do ID do seu projeto!
     region      = southamerica-east1-a
 }
+
+variable "google_credentials_path" {}
+
